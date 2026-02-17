4f86ca7d1dca2757669e53fd94eead6be16ce8c8
Elia Palme <elia.palme@cscs.ch>
2026-02-17 17:29:44 +0100

    add usage example

---
+++ examples/alps-ml-vetting/snippet.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --nodes=2
+#SBATCH --time=0-00:15:00
+#SBATCH --account=csstaff
+
+
+srun  -N ${SLURM_JOB_NUM_NODES} --tasks-per-node=4 --environment=/users/palmee/vetnode/env.toml --network=disable_rdzv_get --container-writable bash -c '
+
+    if [ "${SLURM_LOCALID}" = "0" ]; then
+        mkdir -p /temp/vetnode-${SLURM_JOB_ID}
+        cd /temp/vetnode-${SLURM_JOB_ID}
+        wget -q -O config.yaml https://raw.githubusercontent.com/theely/vetnode/refs/heads/main/examples/alps-ml-vetting/config.yaml
+        python -m venv --system-site-packages .venv
+        source .venv/bin/activate
+        pip install -q --no-cache-dir --index-url "https://jfrog.svc.cscs.ch/artifactory/api/pypi/pypi-remote/simple" vetnode
+        touch /temp/vetnode-${SLURM_JOB_ID}/.setup_done
+    else
+        while [ ! -f /temp/vetnode-${SLURM_JOB_ID}/.setup_done ]; do
+            sleep 2
+        done
+        cd /temp/vetnode-${SLURM_JOB_ID}
+        source .venv/bin/activate
+    fi
+
+    vetnode diagnose config.yaml
+
+    if [ "${SLURM_LOCALID}" = "0" ]; then
+        sleep 5
+        rm -rf /temp/vetnode-${SLURM_JOB_ID}
+    fi
+'
