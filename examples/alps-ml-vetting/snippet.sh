2646c1c2ceb7aa77c519112c39b7fd07df8ed1a0
Elia Palme <elia.palme@cscs.ch>
2026-02-18 14:29:27 +0100

    update snippet example

--- examples/alps-ml-vetting/snippet.sh
+++ examples/alps-ml-vetting/snippet.sh
@@ -4,23 +4,23 @@
 #SBATCH --time=0-00:15:00
 #SBATCH --account=csstaff
 
-export ENV_FILE=/users/palmee/vetnode/env.toml
+export ENV_FILE= # Path to the EDF .toml file
 
 srun  -N ${SLURM_JOB_NUM_NODES} --tasks-per-node=4 --environment=${ENV_FILE} --network=disable_rdzv_get --container-writable bash -c '
 
     if [ "${SLURM_LOCALID}" = "0" ]; then
-        mkdir -p /tmp/vetnode-${SLURM_JOB_ID}
-        cd /tmp/vetnode-${SLURM_JOB_ID}
+        tmpdir=$(mktemp -d)
+        cd ${tmpdir}
         wget -q -O config.yaml https://raw.githubusercontent.com/theely/vetnode/refs/heads/main/examples/alps-ml-vetting/config.yaml
         python -m venv --system-site-packages .venv
         source .venv/bin/activate
         pip install -q --no-cache-dir --index-url "https://jfrog.svc.cscs.ch/artifactory/api/pypi/pypi-remote/simple" vetnode
-        touch /tmp/vetnode-${SLURM_JOB_ID}/.setup_done
+        touch ${tmpdir}/.setup_done
     else
-        while [ ! -f /tmp/vetnode-${SLURM_JOB_ID}/.setup_done ]; do
+        while [ ! -f ${tmpdir}/.setup_done ]; do
             sleep 2
         done
-        cd /tmp/vetnode-${SLURM_JOB_ID}
+        cd ${tmpdir}
         source .venv/bin/activate
     fi
 
@@ -28,6 +28,6 @@ srun  -N ${SLURM_JOB_NUM_NODES} --tasks-per-node=4 --environment=${ENV_FILE} --n
 
     if [ "${SLURM_LOCALID}" = "0" ]; then
         sleep 5
-        rm -rf /tmp/vetnode-${SLURM_JOB_ID}
+        rm -rf ${tmpdir}
     fi
 '
